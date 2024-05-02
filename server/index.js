4fe3beac991d76c8ba8459fc3675eabd078b5979
Chamoli <godchamoli@gmail.com>
2024-05-02 19:49:34 +0530

    Update build script in package.json to include client build and fix production flag

--- server/index.js
+++ server/index.js
@@ -17,17 +17,11 @@ const app = express();
 
 const __dirname1 = path.resolve();
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "/client/dist")));
+app.use(express.static(path.join(__dirname1, "/client/dist")));
 
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "client", "dist", "index.html"))
-  );
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running..");
-  });
-}
+app.get("*", (req, res) =>
+  res.sendFile(path.resolve(__dirname1, "client", "dist", "index.html"))
+);
 
 const express_server = app.listen(
   port,
