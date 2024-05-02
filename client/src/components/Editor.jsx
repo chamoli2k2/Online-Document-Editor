0e8485546e99a4284118d46917950cfeae7ada46
Chamoli <godchamoli@gmail.com>
2024-05-02 14:43:04 +0530

    Remove unused dependencies and configuration files

--- client/src/components/Editor.jsx
+++ client/src/components/Editor.jsx
@@ -50,7 +50,7 @@ const Editor = () => {
   }, []);
 
   useEffect(() => {
-    const socketServer = io("https://online-document-editor-1.onrender.com/");
+    const socketServer = io("http://localhost:9000");
 
     setSocket(socketServer);
 
